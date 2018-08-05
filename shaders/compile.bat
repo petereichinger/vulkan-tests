233c5fc3eb8c3f0d4f8ef1e4510c6e32d105c980
Peter Eichinger <peter.eichinger@gmail.com>
2018-08-05 15:38:24 +0200

    Only auto build shaders on unix systems for now

---
+++ shaders/compile.bat
@@ -0,0 +1,2 @@
+%VULKAN_SDK%\bin\glslangValidator -V triangle.vert
+%VULKAN_SDK%\bin\glslangValidator -V triangle.frag
--- shaders/compile.sh
+++ shaders/compile.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-
-echo HELLO
 $VULKAN_SDK/bin/glslangValidator -V triangle.vert
 $VULKAN_SDK/bin/glslangValidator -V triangle.frag
