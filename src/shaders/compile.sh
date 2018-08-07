a3611b30489aaa0068452e1aa0c7b9cf6cd733bf
Peter Eichinger <peter.eichinger@gmail.com>
2018-08-07 12:11:45 +0200

    Fix compile path

--- src/shaders/compile.sh
+++ src/shaders/compile.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo $(pwd)
-$VULKAN_SDK/bin/glslangValidator -V ../src/shaders/triangle.vert -o shaders/vert.spv
-$VULKAN_SDK/bin/glslangValidator -V ../src/shaders/triangle.frag -o shaders/frag.spv
+$VULKAN_SDK/bin/glslangValidator -V ../../src/shaders/triangle.vert -o vert.spv
+$VULKAN_SDK/bin/glslangValidator -V ../../src/shaders/triangle.frag -o frag.spv
