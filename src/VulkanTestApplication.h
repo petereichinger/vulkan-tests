61a355d88b0e71dd6fcd21338fc6760cbc4537db
Peter Eichinger <peter.eichinger@gmail.com>
2018-08-09 09:47:41 +0200

    Use " for glm includes

--- src/VulkanTestApplication.h
+++ src/VulkanTestApplication.h
@@ -12,8 +12,8 @@
 #include <limits>
 
 #define GLM_FORCE_RADIANS
-#include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
 
 #include <chrono>
 
