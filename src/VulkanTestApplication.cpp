b8ebb8f9c4c29862904d87f4635ff414765d4eb6
Peter Eichinger <peter.eichinger@fau.de>
2018-08-14 12:11:50 +0200

    Cleanup

--- src/VulkanTestApplication.cpp
+++ src/VulkanTestApplication.cpp
@@ -137,7 +137,7 @@ void VulkanTestApplication::createInstance() {
 
     VkApplicationInfo appInfo = {};
     appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
-    appInfo.pApplicationName = "Hello Triangle";
+    appInfo.pApplicationName = "Vulkan Test";
     appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
     appInfo.pEngineName = "No Engine";
     appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
--- src/VulkanTestApplication.h
+++ src/VulkanTestApplication.h
@@ -11,6 +11,7 @@
 #include <iostream>
 #include <limits>
 
+
 #define GLM_FORCE_RADIANS
 #define GLM_FORCE_DEPTH_ZERO_TO_ONE
 #include "glm/glm.hpp"
