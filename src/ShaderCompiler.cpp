1ccbac06486afc75d86372430291a09bc4904eee
Peter Eichinger <peter.eichinger@gmail.com>
2018-08-20 17:40:35 +0200

    Add class for shaderloading

---
+++ src/ShaderCompiler.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Peter Eichinger on 20.08.18.
+//
+
+#include "ShaderCompiler.h"
---
+++ src/ShaderCompiler.h
@@ -0,0 +1,14 @@
+//
+// Created by Peter Eichinger on 20.08.18.
+//
+
+#pragma once
+
+#include <glslang/Public/ShaderLang.h>
+#include <SPIRV/GlslangToSpv.h>
+
+class ShaderCompiler {
+
+};
+
+
